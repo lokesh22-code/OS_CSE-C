60399ebbd2b52d09a16488d3213985090825fa0d
Lukka Amara Lokesh <lukkalokeshwork@gmail.com>
2026-03-30 15:35:08 +0530

    Create 6a.c

---
+++ 6a.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int bt[20], pr[20], wt[20], tat[20], n, i, j, temp;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter Burst Time and Priority:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d %d", &bt[i], &pr[i]);
+
+    for(i = 0; i < n; i++) {
+        for(j = i+1; j < n; j++) {
+            if(pr[i] > pr[j]) {
+                temp = pr[i]; pr[i] = pr[j]; pr[j] = temp;
+                temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
+            }
+        }
+    }
+
+    wt[0] = 0;
+    for(i = 1; i < n; i++)
+        wt[i] = wt[i-1] + bt[i-1];
+
+    for(i = 0; i < n; i++)
+        tat[i] = wt[i] + bt[i];
+
+    printf("\nBT\tPR\tWT\tTAT\n");
+    for(i = 0; i < n; i++)
+        printf("%d\t%d\t%d\t%d\n", bt[i], pr[i], wt[i], tat[i]);
+
+    return 0;
+}
